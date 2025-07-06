45e98ad52b021eb83464bfa65171e65bfa0bba61
koubae <58447627+Koubae@users.noreply.github.com>
2025-07-06 22:45:17 +0200

    fix color

--- app/pages/dashboard/session/[id]/start.vue
+++ app/pages/dashboard/session/[id]/start.vue
@@ -147,7 +147,7 @@ onMounted(async () => {
       <div v-else-if="success && sessionConnections.length > 0">
         <button
             @click="addMember"
-            class="absolute cursor-pointer top-0 left-0 z-[9999] px-6 py-3 bg-yellow-600 hover:bg-yellow-400 text-white font-medium rounded-lg transition-colors shadow-lg"
+            class="absolute cursor-pointer top-0 left-0 z-[9999] px-6 py-3 bg-amber-500 hover:bg-amber-200 text-white font-medium rounded-lg transition-colors shadow-lg"
         >
           Add Member ({{ sessionConnections.length }})
         </button>aaa
