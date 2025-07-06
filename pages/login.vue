b03f12e0f40a35ce46d330439c5a432fd71c9c39
koubae <58447627+Koubae@users.noreply.github.com>
2025-07-06 11:28:47 +0200

    Login page improvements

--- pages/login.vue
+++ pages/login.vue
@@ -1,23 +1,39 @@
 <script setup>
 import { ref } from 'vue'
-const username = ref('user_1')
-const password = ref('pass')
-const applicationID = "application_001"
+
+const applicationID = ref()
+const username = ref()
+const password = ref()
 
 const { login, loading, error } = useLogin()
 
 async function submitLogin() {
-  const result = await login(applicationID, username.value, password.value)
+  localStorage.setItem('currentApplicationID', applicationID.value)
+  localStorage.setItem('currentUsername', username.value)
+
+  const result = await login(applicationID.value, username.value, password.value)
   if (result) {
     await navigateTo('/home')
   }
 }
+
+onMounted(() => {
+  const currentApplicationID = localStorage.getItem('currentApplicationID')
+  const currentUsername = localStorage.getItem('currentUsername')
+
+  if (currentApplicationID) applicationID.value = currentApplicationID
+  if (currentUsername) username.value = currentUsername
+})
+
+
 </script>
 
 <template>
   <div class="login-form">
     <h2>Login</h2>
-    <form @submit.prevent="submitLogin">
+    <form @submit.prevent="submitLogin"
+    >
+      <input v-model="applicationID" type="text" placeholder="Application ID"  required />
       <input v-model="username" type="text" placeholder="Username" required />
       <input v-model="password" type="password" placeholder="Password" required />
       <button type="submit" :disabled="loading">Login</button>
